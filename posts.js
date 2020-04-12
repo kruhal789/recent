9eac16ec7db23061fe7c523988fb481cd66c263f
kruhal789 <63550044+kruhal789@users.noreply.github.com>
2020-04-12 20:30:43 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,3 @@
+var homePage = "https://gfybtru6dyvt.blogspot.com/",
+numPosts = 8;
+function recentPosts(a){if(document.getElementById("recent-posts")){var e=a.feed.entry,title,link,content="",ct=document.getElementById("recent-posts");ct.async=true;for(var i=0;i<numPosts;i++){for(var j=0;j<numPosts;j++){if(e[i].link[j].rel=="alternate"){link=e[i].link[j].href;break}}var title=e[i].title.$t;content+='<li class="recent-posts"><a href="'+link+'" title="'+title+'">'+title+'</a></li>'}ct.innerHTML=content}}var rcp=document.createElement('script');rcp.async=true;rcp.src='/feeds/posts/summary?alt=json-in-script&orderby=published&max-results='+numPosts+'&callback=recentPosts';document.getElementsByTagName('head')[0].appendChild(rcp);
